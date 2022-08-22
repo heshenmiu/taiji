dac8b51232005dfcf4997690a2ccf92aaa22d8cc
Moe <18525355@qq.com>
2022-08-22 16:12:41 +0800

    plugin update

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -184,7 +184,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52738" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53193" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -360,4 +360,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=dist/main.1f19ae8e.js.map
+//# sourceMappingURL=main.1f19ae8e.js.map
